2f511169918f49630741558edc10d6f695174376
thohid44 <thohidislam44@gmail.com>
2023-08-13 00:44:07 +0600

    12 tarik

--- lib/pages/TripPages/views/give_A_Ride.dart
+++ lib/pages/TripPages/views/give_A_Ride.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:bus/Api_services/base_url.dart';
@@ -8,15 +9,22 @@ import 'package:bus/Widget/custom_text_field.dart';
 import 'package:bus/pages/Ship/views/shipPage.dart';
 import 'package:bus/pages/TripPages/Controller/TripController.dart';
 import 'package:bus/pages/TripPages/model/trips_search_model.dart';
+import 'package:bus/pages/TripPages/views/please_search.dart';
 import 'package:bus/pages/TripPages/views/trip_search_page.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:google_place/google_place.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class GiveARide extends StatelessWidget {
+class GiveARide extends StatefulWidget {
+  @override
+  State<GiveARide> createState() => _GiveARideState();
+}
+
+class _GiveARideState extends State<GiveARide> {
   final TextEditingController search = TextEditingController();
 
   List<DropdownMenuItem<String>> get dropdownItem {
@@ -37,8 +45,6 @@ class GiveARide extends StatelessWidget {
     return desRedious;
   }
 
-
-
   List<DropdownMenuItem<String>> get selectVehicle {
     List<DropdownMenuItem<String>> destination = [
       const DropdownMenuItem(child: Text("select"), value: "select"),
@@ -48,15 +54,23 @@ class GiveARide extends StatelessWidget {
   }
 
   String vehicle = "select";
+  bool searchStatus = false;
   var startkm = '';
+
   var deskm = '';
 
   var startRadius = '';
+
   var desRadius = '';
+
   var startPoints = '';
+
   var destionaPoint = '';
+
   var tripContrller = Get.put(TripController());
+
   final _box = GetStorage();
+
   tripSearch() async {
     var token = _box.read(LocalStoreKey.token);
     print("search token $token");
@@ -82,28 +96,209 @@ class GiveARide extends StatelessWidget {
     }
   }
 
+// Search Place
+  final _startSearchFieldController = TextEditingController();
+  final _endSearchFieldController = TextEditingController();
+
+  DetailsResult? startPosition;
+  DetailsResult? endPosition;
+
+  late FocusNode startFocusNode;
+  late FocusNode endFocusNode;
+
+  late GooglePlace googlePlace;
+  List<AutocompletePrediction> predictions = [];
+  Timer? _debounce;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    String apiKey = 'AIzaSyDLMJOClhhQjkfepu0R8iOCIt7bUpUF0nU';
+    googlePlace = GooglePlace(apiKey);
+
+    startFocusNode = FocusNode();
+    endFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    startFocusNode.dispose();
+    endFocusNode.dispose();
+  }
+
+  void autoCompleteSearch(String value) async {
+    var result = await googlePlace.autocomplete.get(value);
+    if (result != null && result.predictions != null && mounted) {
+      print(result.predictions!.first.description);
+      setState(() {
+        predictions = result.predictions!;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
       children: [
         SizedBox(
           height: 20.h,
+        ),
+        TextField(
+          controller: _startSearchFieldController,
+          autofocus: false,
+          focusNode: startFocusNode,
+          style: TextStyle(fontSize: 24),
+          decoration: InputDecoration(
+              hintText: 'Starting Point',
+              hintStyle:
+                  const TextStyle(fontWeight: FontWeight.w500, fontSize: 24),
+              filled: true,
+              fillColor: Colors.grey[200],
+              border: InputBorder.none,
+              suffixIcon: _startSearchFieldController.text.isNotEmpty
+                  ? IconButton(
+                      onPressed: () {
+                        setState(() {
+                          predictions = [];
+                          _startSearchFieldController.clear();
+                        });
+                      },
+                      icon: Icon(Icons.clear_outlined),
+                    )
+                  : null),
+          onChanged: (value) {
+            if (_debounce?.isActive ?? false) _debounce!.cancel();
+            _debounce = Timer(const Duration(milliseconds: 1000), () {
+              if (value.isNotEmpty) {
+                //places api
+                autoCompleteSearch(value);
+              } else {
+                //clear out the results
+                setState(() {
+                  predictions = [];
+                  startPosition = null;
+                });
+              }
+            });
+          },
+        ),
+        SizedBox(height: 10),
+        TextField(
+          controller: _endSearchFieldController,
+          autofocus: false,
+          focusNode: endFocusNode,
+          enabled: _startSearchFieldController.text.isNotEmpty &&
+              startPosition != null,
+          style: TextStyle(fontSize: 24),
+          decoration: InputDecoration(
+              hintText: 'End Point',
+              hintStyle:
+                  const TextStyle(fontWeight: FontWeight.w500, fontSize: 24),
+              filled: true,
+              fillColor: Colors.grey[200],
+              border: InputBorder.none,
+              suffixIcon: _endSearchFieldController.text.isNotEmpty
+                  ? IconButton(
+                      onPressed: () {
+                        setState(() {
+                          predictions = [];
+                          _endSearchFieldController.clear();
+                        });
+                      },
+                      icon: Icon(Icons.clear_outlined),
+                    )
+                  : null),
+          onChanged: (value) {
+            if (_debounce?.isActive ?? false) _debounce!.cancel();
+            _debounce = Timer(const Duration(milliseconds: 1000), () {
+              if (value.isNotEmpty) {
+                //places api
+                autoCompleteSearch(value);
+              } else {
+                //clear out the results
+                setState(() {
+                  predictions = [];
+                  endPosition = null;
+                });
+              }
+            });
+          },
+        ),
+        SizedBox(
+          height: 10.h,
+        ),
+        ListView.builder(
+            shrinkWrap: true,
+            itemCount: predictions.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: CircleAvatar(
+                  child: Icon(
+                    Icons.pin_drop,
+                    color: Colors.white,
+                  ),
+                ),
+                title: Text(
+                  predictions[index].description.toString(),
+                ),
+                onTap: () async {
+                  final placeId = predictions[index].placeId!;
+                  final details = await googlePlace.details.get(placeId);
+                  if (details != null && details.result != null && mounted) {
+                    if (startFocusNode.hasFocus) {
+                      setState(() {
+                        startPosition = details.result;
+                        _startSearchFieldController.text =
+                            details.result!.name!;
+                        predictions = [];
+                      });
+                    } else {
+                      setState(() {
+                        endPosition = details.result;
+                        _endSearchFieldController.text = details.result!.name!;
+                        predictions = [];
+                      });
+                    }
+
+                    if (startPosition != null && endPosition != null) {
+                      print('navigate');
+                      // Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(
+                      //     builder: (context) => MapScreen(),
+                      //   ),
+                      // );
+                    }
+                  }
+                },
+              );
+            }),
+        SizedBox(
+          height: 10.h,
         ),
         Container(
           margin: EdgeInsets.symmetric(horizontal: 20.w),
           child: Row(
             children: [
               Container(
-                width: 135.w,
-                child: CustomTextField(
-                    txt: "Start Point",
-                    label: "Start Point",
-                    hint: "Start Point",
-                    onChange: (t) {
-                      startPoints = t;
-                      print("Start Point $startPoints");
-                    }),
-              ),
+                  width: 135.w,
+                  child: TextField(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => SearchScreen(),
+                            fullscreenDialog: true),
+                      );
+                      //    showPlaces(context: context);
+                    },
+                    autofocus: false,
+                    showCursor: false,
+                    decoration: InputDecoration(),
+                  )),
               SizedBox(
                 width: 10.w,
               ),
@@ -115,7 +310,7 @@ class GiveARide extends StatelessWidget {
                     hint: "Radius",
                     onChange: (t) {
                       startRadius = t;
-                       print("Start Radius $startRadius");
+                      print("Start Radius $startRadius");
                     }),
               ),
               SizedBox(
@@ -137,7 +332,7 @@ class GiveARide extends StatelessWidget {
                   value: startPoint,
                   onChanged: (t) {
                     startkm = t!;
-                  print("Start Km $startkm");
+                    print("Start Km $startkm");
                   },
                   items: dropdownItem,
                 ),
@@ -174,7 +369,7 @@ class GiveARide extends StatelessWidget {
                     hint: "Radius",
                     onChange: (t) {
                       desRadius = t;
-                          print("Des Radius $desRadius");
+                      print("Des Radius $desRadius");
                     }),
               ),
               SizedBox(
@@ -279,8 +474,12 @@ class GiveARide extends StatelessWidget {
               GestureDetector(
                 onTap: () {
                   tripSearch();
+                  setState(() {
+                    searchStatus = true; 
+                  });
                 },
                 child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 50.w),
                     alignment: Alignment.center,
                     child: Text(
                       "Search",
@@ -308,7 +507,7 @@ class GiveARide extends StatelessWidget {
         SizedBox(
           height: 10.h,
         ),
-        FutureBuilder(
+        searchStatus ==true? FutureBuilder(
             future: tripSearch(),
             builder: ((context, AsyncSnapshot snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
@@ -321,6 +520,7 @@ class GiveARide extends StatelessWidget {
               }
               return Expanded(
                 child: ListView.builder(
+                    shrinkWrap: true,
                     itemCount: snapshot.data.data.length,
                     itemBuilder: (context, index) {
                       return Card(
@@ -364,8 +564,31 @@ class GiveARide extends StatelessWidget {
                       );
                     }),
               );
-            }))
+            })): Container()
       ],
     );
+  }
+
+  showPlaces({required context}) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+              elevation: 5,
+              clipBehavior: Clip.hardEdge,
+              //  shadowColor: yelloColor,
+              // title: Text("Success"),
+              contentPadding: EdgeInsets.all(8.0.h),
+              actionsAlignment: MainAxisAlignment.center,
+              content: Container(
+                  alignment: Alignment.center,
+                  height: 120.h,
+                  padding: EdgeInsets.only(top: 10.h),
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(15.r)),
+                  child: Column(
+                    children: [],
+                  )));
+        });
   }
 }
