4e33d5e5900ea73d27ab8c36d847c3c2d7a86f23
rifathossain82 <officialrifat82@gmail.com>
2023-08-17 17:34:25 +0600

    17 tarik 6 ta

---
+++ lib/pages/TripPages/views/trip_make_offer.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TripMakeOffer extends StatefulWidget {
+  const TripMakeOffer({super.key});
+
+  @override
+  State<TripMakeOffer> createState() => _TripMakeOfferState();
+}
+
+class _TripMakeOfferState extends State<TripMakeOffer> {
+  @override
+  Widget build(BuildContext context) {
+    return  SafeArea(child: Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    ),);
+
+  }
+}
